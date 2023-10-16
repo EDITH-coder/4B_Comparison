57fb0b6ac7deb8e5d3edaaca6c62d5dfc938f7c4
EDITH_Coder <edithmashapo@gmail.com>
2023-10-16 12:53:47 +0200

    Add project files.

---
+++ 4B_Comparison/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsMultiplicative(3));
+            Console.WriteLine(IsMultiplicative(14));
+            Console.WriteLine(IsMultiplicative(12));
+            Console.WriteLine(IsMultiplicative(37));
+
+            Console.ReadLine();
+
+        }
+        //Method to check if a given positive number is a multiple of 3 or 7.
+        public static bool IsMultiplicative(int num1)
+        {
+            if(num1 % 3 ==0 || num1 % 7==0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
